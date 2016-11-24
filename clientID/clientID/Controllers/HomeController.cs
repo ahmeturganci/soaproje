4b8329a8975e2443ac4369af66dcddee31085968
AhmetU <aahmet.272@gmail.com>
2016-11-24 18:32:15 +0300

    Giriş ve kayıt ekranı yapıldı.

--- clientID/clientID/Controllers/HomeController.cs
+++ clientID/clientID/Controllers/HomeController.cs
@@ -13,18 +13,12 @@ namespace clientID.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Giris()
         {
-            ViewBag.Message = "Your application description page.";
-
+            
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+      
     }
 }
