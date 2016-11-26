35350915503ce5428b02a809a2d00debb392769b
AhmetU <aahmet.272@gmail.com>
2016-11-26 13:21:27 +0300

    Servis yazılmaya başlandı.

---
+++ serviceID/serviceID/BL/KullaniciIslemler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using serviceID.Model.DataModel;
+using serviceID.Model.ViewModel;
+
+namespace serviceID.BL
+{
+    public class KullaniciIslemler
+    {
+        public static string KullaniciKaydet( Kullanici kullanici)
+        {
+
+            try
+            {
+
+                return "Başarılı";
+            } 
+            catch (Exception ex)
+            {
+
+                
+                return "Basarisiz" + ex.Message;
+            }
+        }
+        public static string GirisYap(string kulAd,string sifre)
+        {
+            try
+            {
+
+                EntityModel db = new EntityModel();
+                // Sınıfı Oluşturamıyorum
+                var kul = (from k in db.tblKullanici
+                           where k.KullaniciAdi == kulAdi && k.Sifre == sifre
+                           select k).SingleOrDefault();
+                if (kul == null)
+                {
+                    return "Şifre ya da kullanıcı adı yanlış.";
+                }
+                else
+                    return "Giriş başarılı!";
+            }
+            catch (Exception ex)
+            {
+                return "İşlem başarısız" + ex.Message;
+            }
+        }
+    }
+}
---
+++ serviceID/serviceID/Model/ViewModel/Kullanici.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using serviceID.Model.DataModel;
+using serviceID.Model.ViewModel;
+
+namespace serviceID.Model.ViewModel
+{
+    public class Kullanici
+    {
+        public int kullaniciId { get; set; }
+        public string kullaniciAdi { get; set; }
+        public string email { get; set; }
+        public string sifre { get; set; }
+
+        public static Kullanici MapData(kullanici k)
+        {
+            Kullanici kul = new Kullanici()
+            {
+                kullaniciId = k.kullaniciId,
+                email = k.email,
+                sifre = k.sifre
+            };
+            return kul;
+        }
+        public static kullanici MapData(Kullanici k)
+        {
+            kullanici tk = new kullanici()
+            {
+                kullaniciId = k.kullaniciId,
+                email = k.email,
+                sifre = k.sifre
+            };
+            return tk;
+        }
+        public static List<Kullanici> MapData(List<kullanici> kulList)
+        {
+            List<Kullanici> liste = new List<Kullanici>();
+
+            foreach (var k in kulList)
+            {
+                liste.Add(MapData(k));
+            }
+
+            return liste;
+        }
+
+        public static List<kullanici> MapData(List<Kullanici> kulList)
+        {
+            List<kullanici> liste = new List<kullanici>();
+
+            foreach (var k in kulList)
+            {
+                liste.Add(MapData(k));
+            }
+
+            return liste;
+        }
+    }
+}
+}
